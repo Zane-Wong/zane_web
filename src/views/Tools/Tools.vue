56414b104cfbb3ad05b2aee3807882492cef5b89
zane-wong <mail_zhuo@163.com>
2024-11-11 03:39:28 +0800

    feat: 增加首页展示内容

--- src/views/Tools/Tools.vue
+++ src/views/Tools/Tools.vue
@@ -1,8 +1,30 @@
 <template>
-    <section id="tools">
-        <SectionTitle title="工具"></SectionTitle>
+    <section id="tools" >
+        <SectionTitle title="实用工具"></SectionTitle>
+        <div class="layout-flex-wrap">
+            <Card v-for="d in tools" :data="d"></Card>
+        </div>
+        <SectionTitle title="教程资料"></SectionTitle>
+        <div class="layout-flex-wrap">
+            <Card v-for="l in learningResources" :data="l"></Card>
+        </div>
+        <SectionTitle title="素材资源"></SectionTitle>
+        <div class="layout-flex-wrap">
+            <Card v-for="d in designResources" :data="d"></Card>
+        </div>
     </section>
 </template>
 <script setup>
 import SectionTitle from '@/components/SectionTitle.vue';
+import Card from "@/components/Card/Card.vue";
+import { tools,learningResources,designResources } from "./data.js";
+
 </script>
+<style scoped>
+.layout-flex-wrap{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: calc(1*var(--margin-large));
+}
+</style>/
---
+++ src/views/Tools/data.js
@@ -0,0 +1,154 @@
+export const tools = [
+    {
+        title: 'v0.dev',
+        coverImg:'https://picx.zhimg.com/80/v2-8065b10a0e1b5eea6b7a618c0e4cfbdb_1440w.jpeg',
+        url:'https://v0.dev/',
+        subTitle: '「前端组件代码生成工具」',
+        description: 'v0是Vercel推出的一款AI工具，可以可使用自然语言描述需求，生成前端代码。',
+        tags:["AI大模型","前端生成","v0.dev"],
+        platform:'在线AI模型'
+    },
+    {
+        title: 'RegExr.com',
+        coverImg:'https://www.runoob.com/wp-content/uploads/2014/03/20f5a98835b67f6159d11.png',
+        url:'https://regexr.com/',
+        subTitle: '在线正则表达式学习调试工具',
+        description: '一个集学习、构建和测试正则表达式（RegEx/)于一身的在线工具。',
+        tags:["正则","RegExp","regexr.com"],
+        platform:'在线工具'
+    },
+    {
+        title: 'draw.io',
+        coverImg:'https://www.yangqi.show/images/drawio-tips/cover.png',
+        url:'https://app.diagrams.net/',
+        subTitle: 'UML画图建模工具',
+        description: '一款强大且支持在线编辑和导出的流程图绘制神器。',
+        tags:["UML建模","免费画图","Draw.io"],
+        platform:'Web+桌面端'
+    },
+    {
+        title: '百度脑图',
+        coverImg:'https://so1.360tres.com/t0175f66c57b6472bb3.png',
+        url:'https://naotu.baidu.com/',
+        subTitle: '百度出的免费思维导图工具',
+        description: '百度公司旗下的免费思维导图工具网站，支持自动实时保存，业界良心。',
+        tags:["脑图","实时保存","naotu.baidu"],
+        platform:'在线工具'
+    },
+    {
+        title: 'TS演练场',
+        coverImg:'https://media2.dev.to/dynamic/image/width=1000,height=420,fit=cover,gravity=auto,format=auto/https%3A%2F%2Fdev-to-uploads.s3.amazonaws.com%2Fuploads%2Farticles%2Fkioot4ilontcqokek526.png',
+        url:'https://www.typescriptlang.org/zh/play/',
+        subTitle: 'TS+JS演练场',
+        description: 'TS在线编译环境，同时可以转换TS和JS代码，直观对比学习两种语言。',
+        tags:["TS演练场","typescriptlang.org"],
+        platform:'在线工具'
+    },
+    {
+        title: 'AirPortal',
+        coverImg:'https://maorx.cn/images/workLinkPic_AirPortal.jpg',
+        url:'https://www.airportal.cn/',
+        subTitle: '在线文件快传',
+        description: '只要有浏览器，在任意设备之间设备传送文件，完全免费使用',
+        tags:["文件快传",'airportal.cn'],
+        platform:'在线工具'
+    },
+    {
+        title: '在线SVG编辑器',
+        coverImg:'https://www.runoob.com/wp-content/uploads/2013/06/runoob.jpeg',
+        url:'https://www.jyshare.com/more/svgeditor/',
+        subTitle: '在线SVG编辑',
+        description: '轻松在线编辑自己需要的矢量图形，通过视图的源码选项可以看到SVG源码，非常方便。',
+        tags:["矢量SVG图形",'在线编辑'],
+        platform:'在线工具'
+    },
+    {
+        title: 'PixPin',
+        coverImg:'https://cdn.utgd.net/images/PixPin_%E9%A2%98%E5%9B%BE.png',
+        url:'https://pixpinapp.com/',
+        subTitle: '在线SVG编辑',
+        description: '截图固定屏幕、OCR、长截图，集Snipaste、QQ、ScreenToGif优点于一身的全能神器！',
+        tags:["截图固定","长截图",'GIF'],
+        platform:'桌面应用'
+    },
+    {
+        title: 'Geek Uninstaller',
+        coverImg:'https://www.fy6b.com/wp-content/uploads/2021/11/2021112412353875.jpg',
+        url:'https://geekuninstaller.com/',
+        subTitle: '极简Windows软件卸载工具',
+        description: '功能强大，界面简洁，小巧实用的免费卸载软件，卸载自带清理注册表，很推荐！',
+        tags:["卸载","清注册表","geekuninstaller"],
+        platform:'Windows软件'
+    },
+    {
+        title: '欧路词典',
+        coverImg:'https://pica.zhimg.com/70/v2-2ab6daf848684abf968c579a40def6f5_1440w.avis?source=172ae18b&biz_tag=Post',
+        url:'https://dict.eudic.net/',
+        subTitle: '优秀的国产词典软件',
+        description: '一款支持多设备的词典软件，设置快捷键Ctrl+m迷你查词非常好用，程序员必备，也支持『自定义导入mdd/mdx格式词典』。',
+        tags:["迷你查词","跨端","dict.eudic.net"],
+        platform:'全平台'
+    },
+]
+
+// 教程资料
+export let learningResources = [
+    {
+        title: '书栈网',
+        coverImg:'https://www.bookstack.cn/static/images/logo.png',
+        url:'https://www.bookstack.cn/',
+        subTitle: '书栈网是一个专注于技术书籍的社区平台',
+        description: '书栈网是一个专注于技术书籍的社区平台，涵盖了上百种技术类的教程文档资料。',
+        tags:["书籍","文档","bookstack.cn"],
+        platform:'全平台'
+    },
+    {
+        title: '廖雪峰的官方网站',
+        coverImg:'https://liaoxuefeng.com/books/javascript/introduction/cover.jpg',
+        url:'https://liaoxuefeng.com/',
+        subTitle: '廖雪峰的个人网站',
+        description: '技术无止境，学习到终身，2024年了廖老师还在更新自己的网站，绝对是全栈技术人的典范，教程内容通俗易懂，顶！',
+        tags:["廖雪峰","个人网站","liaoxuefeng"],
+        platform:'在线网站'
+    },
+    {
+        title: 'W3school',
+        coverImg:'https://www.w3school.com.cn/ui2019/bg.png',
+        url:'https://www.w3school.com.cn/',
+        subTitle: '免费Web技术网站',
+        description: 'HTML到CSS至进阶的 XML、SQL、JS、PHP 和 ASP.NET。涵盖W3C标准的技术教程。',
+        tags:["网站技术","w3school"],
+        platform:'在线网站'
+    },
+    {
+        title: '菜鸟教程',
+        coverImg:'https://www.runoob.com/wp-content/uploads/2013/06/runoob.jpeg',
+        url:'https://www.runoob.com/',
+        subTitle: '内容更广泛的技术教程网站',
+        description: '从前端到后端、从语言到数据结构、从算法到设计模式，都有相关资料可以学习，内容丰富通俗易懂。',
+        tags:["前后端","框架","设计模式"],
+        platform:'在线网站'
+    }
+]
+
+// 素材资源
+export let designResources = [
+    {
+        title: 'iconfont',
+        coverImg:'https://www.logosc.cn/uploads/articles/2022/08/24/16613279616371956.png',
+        url:'https://www.jyshare.com/more/svgeditor/',
+        subTitle: '免费字体图标库',
+        description: '阿里巴巴开发的免费字体图标库，收录了海量图标字体，非常良心',
+        tags:["图标库","设计资源",'iconfont.cn'],
+        platform:'在线资源'
+    },
+    {
+        title: 'ZCOOL',
+        coverImg:'https://img.zcool.cn/community/special_cover/610058c7e54d0000017122ca4a6f.jpg',
+        url:'https://www.zcool.com.cn/',
+        subTitle: '站酷设计师资源',
+        description: '聚集海量优秀设计师，分享创意和优秀作品，丰富的素材资源，为每个人带来创作灵感。',
+        tags:["设计师资源",'zcool.com'],
+        platform:'Web平台'
+    }
+]
