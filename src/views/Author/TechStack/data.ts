fb477e214bb23adf1d77e031d84e66c8e41f38a0
zane-wong <mail_zhuo@163.com>
2024-11-11 03:37:03 +0800

    fix: 技术栈内容

--- src/views/Author/TechStack/data.ts
+++ src/views/Author/TechStack/data.ts
@@ -1,53 +1,53 @@
 export const techs = [
     [
         {
-            name: 'Vue',
+            name: 'C++',
             exp: 20,
-            img: 'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'
+            img: 'https://img.imgdb.cn/item/604ad6bc5aedab222c86fef8.png'
         },
         {
-            name: 'Vue',
+            name: 'Java',
             exp: 40,
-            img: 'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'
+             img: 'https://www.diggita.it/modules/auto_thumb/2017/01/02/1573761_java-960x576_thumb_big.jpg'//'https://brandlogos.net/wp-content/uploads/2021/11/java-logo.png'
         },
         {
-            name: 'Vue',
+            name: 'MySQL',
             exp: 60,
-            img: 'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'
+            img: 'https://zixzax.net/wp-content/uploads/2020/05/MySQL-%E0%B8%84%E0%B8%B7%E0%B8%AD%E0%B8%AD%E0%B8%B0%E0%B9%84%E0%B8%A3.png'
         }
     ],
     [
         {
-            name: 'Vue',
+            name: 'Node.js',
             exp: 90,
-            img: 'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'
+            img: 'https://colorlib.com/wp/wp-content/uploads/sites/2/node.js-logo.png'
         },
         {
             name: 'Vue',
             exp: 95,
-            img: 'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'
+            img: 'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'
         },
         {
-            name: 'Vue',
+            name: '小程序',
             exp: 100,
-            img: 'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'
+            img: 'https://tse2-mm.cn.bing.net/th/id/OIP-C.h-JlocD6xYUFpv3Fz5rrRwHaHa?rs=1&pid=ImgDetMain'
         }
     ],
     [
         {
-            name: 'Vue',
+            name: 'CSS',
             exp: 90,
-            img: 'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'
+            img: 'https://tse1-mm.cn.bing.net/th/id/OIP-C.wmpztFuagBhk8T7esKos-wHaKc?rs=1&pid=ImgDetMain'
         },
         {
-            name: 'Vue',
+            name: 'JavaScript',
             exp: 95,
-            img: 'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'
+            img: 'https://cdn.pixabay.com/photo/2015/04/23/17/41/javascript-736400_1280.png'
         },
         {
-            name: 'Vue',
+            name: 'HTML',
             exp: 100,
-            img: 'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'
+            img: 'https://emaillistvalidation.com/blog/content/images/2023/09/HTML5_logo_and_wordmark.svg.png'
         }
     ]
 ]
