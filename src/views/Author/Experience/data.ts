9e57b395b78e25984e0b4afc5145d66f4e7f83a8
zane-wong <mail_zhuo@163.com>
2024-11-11 06:18:13 +0800

    update: 更新个人经历，增加图片

--- src/views/Author/Experience/data.ts
+++ src/views/Author/Experience/data.ts
@@ -1,80 +1,92 @@
 export let events = [
     {
-        start_time: "2024.06.18",
+        start_time: "2023.09.06",
         end_time: "至今",
-        title: '测开实习',
-        description: '深圳市腾娱互动',
+        title: '硕士研究生',
+        description: '澳门，计算机科学硕士在读',
         imgs:[
-            {desc:'吉祥物',src:'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'},
-            {desc:'工作餐',src:'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'},
-            {desc:'下午茶',src:'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'}
+
+            {desc:'描述',src:'https://img.zcool.cn/community/01iexfpkjk7v2ejgzjugvr3739.jpg?imageMogr2/auto-orient/thumbnail/1280x%3e/sharpen/0.5/quality/100/format/webp'},
+            {desc:'描述',src:'https://fds.cityu.edu.mo/uploads_thumb/page/2cb1d5280b5c67a1d901eb60eb6b583a_1000X1000.jpg'},
+            {desc:'描述',src:'https://login.cityu.edu.mo/cas/images/background.png'}
+
         ]
     },
     {
-        start_time: new Date().toLocaleDateString(),
-        end_time: new Date().toLocaleDateString(),
-        title: '事情',
-        description: '事件的描述',
+        start_time: "2024.06.18",
+        end_time: "2024.09.14",
+        title: '测开实习生',
+        description: '深圳市，腾娱互动科技有限公司',
         imgs:[
-            {desc:'描述',src:'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'},
-            {desc:'描述',src:'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'},
-            {desc:'描述',src:'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'}
+            {desc:'吉祥物',src:'https://public-cdn-tc.mokahr.com/4dc3fa0c-2a97-4b3a-bb77-4b194c1da4ce.jpeg'},
+            {desc:'工作餐',src:'https://public-cdn-tc.mokahr.com/tencent-ieg/866df5d2-a9a4-40f4-9cf7-3ba7f6ac62e6.jpeg'},
+            {desc:'下午茶',src:'https://public-cdn-tc.mokahr.com/tencent-ieg/4d25baf8-735b-4428-861f-3eb34377b0e2.jpeg'}
+
         ]
     },
     {
-        start_time: new Date().toLocaleDateString(),
-        end_time: new Date().toLocaleDateString(),
-        title: '美食美么',
-        description: '事件的描述',
+        start_time: "2019.09.01",
+        end_time: "2023.06.14",
+        title: '本科生',
+        description: '西安市，软件工程本科',
         imgs:[
-            {desc:'描述',src:'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'},
-            {desc:'描述',src:'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'},
-            {desc:'描述',src:'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'}
+            {desc:'校门',src:'https://www.xsyu.edu.cn/__local/3/95/AF/82DBFB153D3B499A269FC6CA77B_2BA5E660_14DB3.jpg?e=.jpg'},
+            {desc:'描述',src:'https://img.picui.cn/free/2024/11/11/67312638a2f4d.jpg'},
+            {desc:'学校',src:'https://img.picui.cn/free/2024/11/11/67312639bc14f.jpg'}
+            //https://img.picui.cn/free/2024/11/11/6731263659265.jpg
+            // https://img.picui.cn/free/2024/11/11/6731263661b10.jpg
+            // https://img.picui.cn/free/2024/11/11/67312636646f1.jpg
+            // https://img.picui.cn/free/2024/11/11/6731263660b60.jpg
+            // https://img.picui.cn/free/2024/11/11/673126365d407.jpg
+            // https://img.picui.cn/free/2024/11/11/67312638a2f4d.jpg
+            // https://img.picui.cn/free/2024/11/11/67312639bc14f.jpg
         ]
+
     },
     {
-        start_time: new Date().toLocaleDateString(),
-        end_time: new Date().toLocaleDateString(),
-        title: '吹牛',
-        description: '事件的描述',
+        start_time: "2019.06.07",
+        end_time: "2019.06.08",
+        title: '高考生',
+        description: '洛南县，高考',
         imgs:[
-            {desc:'描述',src:'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'},
-            {desc:'描述',src:'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'},
-            {desc:'描述',src:'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'}
+            // {desc:'描述',src:'https://so1.360tres.com/t019ade9360bbde8c06.jpg'},
+            {desc:'描述',src:'https://www.luonan.gov.cn/wcm.files/upload/CMSln/202205/202205310219011.png'},
+            {desc:'描述',src:'https://so1.360tres.com/t019c2ace1116f1051b.jpg'}
+
         ]
     },
-    {
-        start_time: new Date().toLocaleDateString(),
-        end_time: new Date().toLocaleDateString(),
-        title: '吹牛',
-        description: '事件的描述',
-        imgs:[
-            {desc:'描述',src:'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'},
-            {desc:'描述',src:'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'},
-            {desc:'描述',src:'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'}
-        ]
-    },
-    {
-        start_time: new Date().toLocaleDateString(),
-        end_time: new Date().toLocaleDateString(),
-        title: '吹牛',
-        description: '事件的描述',
-        imgs:[
-            {desc:'描述',src:'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'},
-            {desc:'描述',src:'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'},
-            {desc:'描述',src:'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'}
-        ]
-    },
-    {
-        start_time: new Date().toLocaleDateString(),
-        end_time: new Date().toLocaleDateString(),
-        title: '吹牛',
-        description: '事件的描述',
-        imgs:[
-            {desc:'描述',src:'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'},
-            {desc:'描述',src:'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'},
-            {desc:'描述',src:'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'}
-        ]
-    }
+    // {
+    //     start_time: new Date().toLocaleDateString(),
+    //     end_time: new Date().toLocaleDateString(),
+    //     title: '吹牛',
+    //     description: '事件的描述',
+    //     imgs:[
+    //         {desc:'描述',src:'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'},
+    //         {desc:'描述',src:'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'},
+    //         {desc:'描述',src:'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'}
+    //     ]
+    // },
+    // {
+    //     start_time: new Date().toLocaleDateString(),
+    //     end_time: new Date().toLocaleDateString(),
+    //     title: '吹牛',
+    //     description: '事件的描述',
+    //     imgs:[
+    //         {desc:'描述',src:'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'},
+    //         {desc:'描述',src:'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'},
+    //         {desc:'描述',src:'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'}
+    //     ]
+    // },
+    // {
+    //     start_time: new Date().toLocaleDateString(),
+    //     end_time: new Date().toLocaleDateString(),
+    //     title: '吹牛',
+    //     description: '事件的描述',
+    //     imgs:[
+    //         {desc:'描述',src:'https://images.viblo.asia/f71e4362-cb61-425b-9993-e75446351f78.jpg'},
+    //         {desc:'描述',src:'https://swansoftwaresolutions.com/wp-content/uploads/2020/01/Benefits-of-Using-VUE.JS.jpeg'},
+    //         {desc:'描述',src:'https://cdn-media-1.freecodecamp.org/ghost/2019/03/vueart.png'}
+    //     ]
+    // }
 
 ]
