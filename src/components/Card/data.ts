e8a99afbd996a8b7ab3d4652f92a5582cfbd5099
zane-wong <mail_zhuo@163.com>
2024-11-11 06:15:56 +0800

    update: 更新作品数据、更新页脚数据

--- src/components/Card/data.ts
+++ src/components/Card/data.ts
@@ -8,7 +8,34 @@ export const projects: any[] = [
         tags:["Vue","Vite","TS"], 
         platform:'Web'
     },
-    { 
+    {
+        title: '校园社区',
+        coverImg:'https://ts1.cn.mm.bing.net/th/id/R-C.1d03a6ccc35b730049fed88775c560f8?rik=jcv81cBAlbqxqQ&riu=http%3a%2f%2fimages.shejidaren.com%2fwp-content%2fuploads%2f2013%2f08%2f031651Tg0.jpg&ehk=NiUZAHHQrNiY98ChYMHANuPymzhXmVpJ0aJUSAWj6q4%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
+        url:'comming soon',
+        subTitle: '子标题',
+        description: '开发中，敬请期待',
+        tags:['uni-app','H5','小程序'],
+        platform:'uni-app'
+    },
+    {
+        title: 'Flappy Bird',
+        coverImg:'https://playfreeonline32.com/wp-content/uploads/Flappy-Bird-Online-%E2%80%A2-Play-Flappy-Bird-Games-for-Free-Today.jpeg',
+        url:'/FlappyBird/FlappyBird.html',
+        subTitle: 'H5游戏',
+        description: '用H5和JavaScript实现的FlappyBird小游戏',
+        tags:['HTML','JS','CSS','游戏'],
+        platform:'H5'
+    },
+    {
+        title: '贪食蛇',
+        coverImg:'https://img.zcool.cn/community/01dcf1554be63a000001bf7268fd65.jpg?imageMogr2/auto-orient/thumbnail/1280x%3e/sharpen/0.5/quality/100/format/webp',
+        url:'/Snake/snake.html',
+        subTitle: 'H5游戏',
+        description: '用H5和JavaScript实现的贪食蛇小游戏',
+        tags:['HTML','JS','CSS','游戏'],
+        platform:'H5'
+    },
+    {
         title: 'QtIM',
         coverImg:'https://tse1-mm.cn.bing.net/th/id/OIP-C.g--KOfpgbwrd9g9t3ruYHAHaFC?rs=1&pid=ImgDetMain',
         url:' ',
@@ -17,33 +44,22 @@ export const projects: any[] = [
         tags:["C++","Qt","MySQL","IM"],
         platform:'Windows'
     },
-    { 
-        title: 'Flappy Bird', 
-        coverImg:'https://playfreeonline32.com/wp-content/uploads/Flappy-Bird-Online-%E2%80%A2-Play-Flappy-Bird-Games-for-Free-Today.jpeg', 
-        url:'/FlappyBird/FlappyBird.html',
-        subTitle: 'H5游戏', 
-        description: '用H5和JavaScript实现的FlappyBird小游戏', 
-        tags:['HTML','JS','CSS','游戏'], 
-        platform:'H5'
-    },
-    { 
-        title: '贪食蛇',
-        coverImg:'https://img.zcool.cn/community/01dcf1554be63a000001bf7268fd65.jpg?imageMogr2/auto-orient/thumbnail/1280x%3e/sharpen/0.5/quality/100/format/webp',
-        url:'/Snake/snake.html',
-        subTitle: 'H5游戏', 
-        description: '用H5和JavaScript实现的贪食蛇小游戏', 
-        tags:['HTML','JS','CSS','游戏'], 
-        platform:'H5'
-    },
-    { 
-        title: '校园社区',
-        coverImg:'https://ts1.cn.mm.bing.net/th/id/R-C.1d03a6ccc35b730049fed88775c560f8?rik=jcv81cBAlbqxqQ&riu=http%3a%2f%2fimages.shejidaren.com%2fwp-content%2fuploads%2f2013%2f08%2f031651Tg0.jpg&ehk=NiUZAHHQrNiY98ChYMHANuPymzhXmVpJ0aJUSAWj6q4%3d&risl=&pid=ImgRaw&r=0&sres=1&sresct=1',
-        url:'comming soon',
-        subTitle: '子标题', 
-        description: '开发中，敬请期待',
-        tags:['uni-app','H5','小程序'],
-        platform:'uni-app'
-    },
+    {
+        title: '“魔镜”小程序',
+        url:'https://gitee.com/zane_wong/wechat-applet.git',
+        coverImg:'https://img.picui.cn/free/2024/11/11/67310f72e5746.png',
+        // https://img.picui.cn/free/2024/11/11/67311477bb151.png
+        // https://img.picui.cn/free/2024/11/11/67310ea2610aa.png
+        // https://img.picui.cn/free/2024/11/11/6731147970872.png
+        // https://img.picui.cn/free/2024/11/11/6731147856aed.png
+        // https://img.picui.cn/free/2024/11/11/673114788e3a5.png
+        // https://img.picui.cn/free/2024/11/11/67311478a53d3.png
+        subTitle: 'AI测肤小程序',
+        description: '参加C4大赛的作品，该小程序可以通过拍照对人脸肤质进行分析，可视化显示各项指标，推荐护肤产品，发表护肤经验等等。',/*开发文档：https://blog.csdn.net/m0_46332820/article/details/126365454*/
+        tags:["小程序","C4大赛","AI测肤"],
+        platform:'小程序'
+    }
+
     // {
     //     title: '标题',
     //     coverImg:'https://feegblog.com/wp-content/uploads/2019/10/logo_vue.png',
@@ -53,13 +69,5 @@ export const projects: any[] = [
     //     tags:[1,2,3],
     //     platform:'平台'
     // },
-    // {
-    //     title: '标题',
-    //     url:'',
-    //     coverImg:'正在开发',
-    //     subTitle: '子标题',
-    //     description: '描述',
-    //     tags:[2,3,4],
-    //     platform:'平台'
-    // }
+
 ];
--- src/components/Foot/data.ts
+++ src/components/Foot/data.ts
@@ -11,17 +11,18 @@ export const about = [
     {
         title: '关于作者',
         data: [
-            {label: '', value: 'mail_zhuo@163.com', type: 'text'},
+            {label: '邮箱', value: '2018925336@qq.com', type: 'text'},
             {label: 'GitHub', value: 'https://github.com/Zane-Wong', type: 'link'},
+            {label: 'Gitee', value: 'https://gitee.com/zane_wong', type: 'link'},
             {label: '个人主页', value: BaseURL, type: 'link'}
         ]
     },
-    // {
-    //     title:'友情链接',
-    //     data:[
-    //         {label:'link1',value:'url1',type:'link'},
-    //         {label:'link2',value:'url2',type:'link'},
-    //         {label:'link3',value:'url3',type:'link'}
-    //     ]
-    // }
+    {
+        title:'友链推荐',
+        data:[
+            {label:'个人主页|Marorx',value:'https://maorx.cn/',type:'link'},
+            {label:'SU7|GameMCU',value:'https://gamemcu.com/su7/',type:'link'},
+            // {label:'link3',value:'url3',type:'link'}
+        ]
+    }
 ]
