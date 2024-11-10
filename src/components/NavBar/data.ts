02c24a5e22853235aa783a8db9eeea7c9ffda75b
zane-wong <mail_zhuo@163.com>
2024-11-11 03:35:54 +0800

    fix: 调整导航栏按钮文本内容

--- src/components/NavBar/data.ts
+++ src/components/NavBar/data.ts
@@ -5,7 +5,7 @@ export const navList : {
 }[] = [
     {title:'首页',url:'/',hash:'#index'},
     // {title:'工具',url:'#tools'},
-    {title:'项目',url:'/projects',hash:'#main'},
+    {title:'作品',url:'/projects',hash:'#main'},
     // {title:'工具',url:'/tools'},
     {title:'博客',url:'/blogs',hash:'#main'},
     // {title:'关于',url:'about'},
