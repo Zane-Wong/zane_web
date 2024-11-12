4f514807ef8ef6cd2e0f9dd6efdd45b8d114ebbe
zane-wong <mail_zhuo@163.com>
2024-11-12 11:17:28 +0800

    fix: 静态部署打包问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url';
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+const noAttr = () => {
+  return {
+    name: "no-attribute",
+    transformIndexHtml(html: string) {
+      html = html.replace(`type="module" crossorigin`, `type="module"`);
+      return html.replace(`crossorigin`,'');
     }
   }
+}
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue(),noAttr()],
+  base: './',
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src',import.meta.url))//
+  //   }
+  // }
 })
